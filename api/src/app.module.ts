19aafb2a4149f5510dfdd617ff4d0ae5ea234472
SamuelSilva-DEVL <samuelmec1@gmail.com>
2026-02-16 15:18:42 -0300

    feat: implement Zod validation for user creation and add environment schema

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -2,13 +2,15 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { PrismaService } from './prisma/prisma.service';
 import { CreateUserController } from './controllers/create-user.controller';
+import { envSchema } from './env';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
+      validate: env => envSchema.parse(env),
       isGlobal: true,
     }),
-  ],
+  ],  
   controllers: [CreateUserController],
   providers: [PrismaService],
 })
--- api/src/controllers/create-user.controller.ts
+++ api/src/controllers/create-user.controller.ts
@@ -1,6 +1,16 @@
-import { Body, ConflictException, Controller, HttpCode, Post } from '@nestjs/common';
+import { Body, ConflictException, Controller, HttpCode, Post, UsePipes } from '@nestjs/common';
 import { hash } from 'bcryptjs';
+import { ZodValidationPipe } from 'src/pipes/zod-validation-pipe';
 import { PrismaService } from 'src/prisma/prisma.service';
+import z from 'zod';
+
+const createUserSchema = z.object({
+    name: z.string().min(1, 'O nome é obrigatório'),
+    email: z.email('O email deve ser válido'),
+    password: z.string().min(6, 'A senha deve conter no mínimo 6 caracteres'),
+})
+
+type CreateUserSchema = z.infer<typeof createUserSchema>
 
 @Controller('/users')
 export class CreateUserController {
@@ -10,7 +20,8 @@ export class CreateUserController {
 
     @Post()
     @HttpCode(201)
-    async createUser(@Body() body: any) {
+    @UsePipes(new ZodValidationPipe(createUserSchema))
+    async createUser(@Body() body: CreateUserSchema) {
         const { name, email, password } = body
 
         const hasExistingUserWithEmail = await this.prisma.user.findUnique({
