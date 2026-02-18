4a139ea2be342c80579d94e251777e995e719490
SamuelSilva-DEVL <samuelmec1@gmail.com>
2026-02-18 15:30:32 -0300

    feat: add FetchListGoalsController to handle goal retrieval with pagination

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -6,6 +6,7 @@ import { envSchema } from "./env"
 import { AuthModule } from "./auth/auth.module"
 import { AuthenticateController } from "./controllers/authenticate.controller"
 import { CreateGoalController } from "./controllers/create-goal.controller"
+import { FetchListGoalsController } from './controllers/fetch-list-goals.controller'
 
 @Module({
   imports: [
@@ -19,6 +20,7 @@ import { CreateGoalController } from "./controllers/create-goal.controller"
     CreateUserController,
     AuthenticateController,
     CreateGoalController,
+    FetchListGoalsController
   ],
   providers: [PrismaService],
 })
---
+++ api/src/controllers/fetch-list-goals.controller.ts
@@ -0,0 +1,39 @@
+import { Controller, Get, Query, UseGuards } from "@nestjs/common"
+import { JwtAuthGuard } from "src/auth/jwt-auth.guard"
+import { ZodValidationPipe } from 'src/pipes/zod-validation-pipe'
+import { PrismaService } from "src/prisma/prisma.service"
+import z from 'zod'
+
+const pageQueryParamSchema = z
+  .string()
+  .optional()
+  .default("1")
+  .transform(Number)
+  .pipe(z.number().min(1))
+
+const queryValidationPipe = new ZodValidationPipe(pageQueryParamSchema)
+
+type PageQueryParamSchema = z.infer<typeof pageQueryParamSchema>
+
+@Controller("/goals")
+@UseGuards(JwtAuthGuard)
+export class FetchListGoalsController {
+  constructor(private prisma: PrismaService) {}
+
+  @Get()
+  async handle(@Query("page", queryValidationPipe) page: PageQueryParamSchema) {
+    const perPage = 10
+
+    const goals = await this.prisma.goal.findMany({
+      take: perPage,
+      skip: (page - 1) * perPage,
+      orderBy: {
+        createdAt: "asc",
+      },
+    })
+
+    return {
+      goals,
+    }
+  }
+}
