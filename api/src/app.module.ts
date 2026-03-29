e537ca34d35e6810f6ca52efe0dd64db18fdac60
SamuelSilva-DEVL <samuelmec1@gmail.com>
2026-03-29 11:38:06 -0300

    refactor: refactor controllers and restructure directory layout

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,12 +1,12 @@
 import { Module } from "@nestjs/common"
 import { ConfigModule } from "@nestjs/config"
 import { PrismaService } from "./prisma/prisma.service"
-import { CreateUserController } from "./controllers/create-user.controller"
+import { CreateUserController } from "./controllers/user/create-user.controller"
 import { envSchema } from "./env"
 import { AuthModule } from "./auth/auth.module"
-import { AuthenticateController } from "./controllers/authenticate.controller"
-import { CreateGoalController } from "./controllers/create-goal.controller"
-import { FetchListGoalsController } from './controllers/fetch-list-goals.controller'
+import { AuthenticateController } from "./controllers/auth/authenticate.controller"
+import { CreateGoalController } from "./controllers/goals/create-goal.controller"
+import { FetchListGoalsController } from "./controllers/goals/fetch-list-goals.controller"
 
 @Module({
   imports: [
@@ -20,7 +20,7 @@ import { FetchListGoalsController } from './controllers/fetch-list-goals.control
     CreateUserController,
     AuthenticateController,
     CreateGoalController,
-    FetchListGoalsController
+    FetchListGoalsController,
   ],
   providers: [PrismaService],
 })
